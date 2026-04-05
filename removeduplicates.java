1b56976b7ee86b7ff0909da502e6fe8887217982
ryeshika07 <yeshika.9124@gmail.com>
2026-04-05 19:34:10 +0530

    Add files via upload

---
+++ removeduplicates.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+class Solution {
+    // Removes duplicates using HashSet and returns count of unique elements
+    public int removeDuplicates(int[] nums) {
+        // HashSet to store unique elements we have seen
+        HashSet<Integer> seen = new HashSet<>();
+
+        // Position to overwrite next unique element
+        int index = 0;
+
+        // Loop over each number in nums
+        for (int num : nums) {
+            // If num is not in the set, it is unique
+            if (!seen.contains(num)) {
+                // Add num to the set
+                seen.add(num);
+
+                // Write num at current index position
+                nums[index] = num;
+
+                // Move index forward
+                index++;
+            }
+        }
+        // Return number of unique elements
+        return index;
+    }
+}
+
+public class removeduplicates {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        Solution sol = new Solution();
+        int k = sol.removeDuplicates(nums);
+
+        System.out.println("k = " + k);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
